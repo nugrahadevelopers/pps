c0eaf0da12d1d93ef0fbc9295605c41944a2b7c3
RenoNugraha <65001915+nugrahadevelopers@users.noreply.github.com>
2023-04-06 09:23:23 +0700

    Initial Commit

--- resources/views/pages/panel/admin/dashboard/index.blade.php
+++ resources/views/pages/panel/admin/dashboard/index.blade.php
@@ -1,13 +1,7 @@
 <x-app-layout>
     <x-slot name="title">{{ __('dashboard.page_title') }}</x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-theme.container>
+        Hallo Selamat Datang {{ auth()->user()->name }}
+    </x-theme.container>
 </x-app-layout>
--- resources/views/pages/panel/admin/transaction/invoice/partials/_show.blade.php
+++ resources/views/pages/panel/admin/transaction/invoice/partials/_show.blade.php
@@ -27,7 +27,7 @@
             @foreach ($order->orderItems as $item)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{ $item->name }}
+                        {{ $item->item_name }}
                     </th>
                     <td class="px-6 py-4">
                         {{ $item->price }}
